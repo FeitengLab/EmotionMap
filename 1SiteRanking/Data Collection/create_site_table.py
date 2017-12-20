63fd19932b08788b742d0dcd00f278f03689f4d7
Yuhao Kang <kkyyhh96@163.com>
2017-12-20 16:54:53 +0800

    kyh-20171120-1 collect flickr photos

---
+++ 1SiteRanking/Data Collection/create_site_table.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+# version:python3.5.1
+# author:kyh
+# import geodata csv files to cloud database
+
+import psycopg2
+
+
+class CloudDatabase(object):
+    # Init database and input ip
+    # 初始化数据库并传入ip
+    def __init__(self, database, user, password, ip="127.0.0.1", port="5432"):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.ip = ip
+        self.port = port
+
+    # Connect database and set input as host, return connection and cursor
+    # 连接ip端数据库并返回connection和cursor
+    def db_connect(self):
+        self.connection = psycopg2.connect(database=self.database, user=self.user,
+                                           password=self.password, host=self.ip, port=self.port)
+        self.cursor = self.connection.cursor()
+
+    # Write log file
+    # 输出日志
+    def write_log(self, e):
+        self.connection.rollback()
+        with open("log.txt", 'a') as log_file:
+            log_file.writelines(str(e))
+
+    # Copy files to database
+    # 将csv文件导入数据库
+    def import_site(self, filepath):
+        with open(filepath,'r') as file:
+            for site in file.readlines():
+                id = site.split('\t')[0]
+                name = site.split('\t')[1]
+                lat = site.split('\t')[2]
+                lon = site.split('\t')[3].split('\n')[0]
+                try:
+                    sql_command="INSERT INTO site VALUES ({0},'{1}',{2},{3});".format(id,name,lat,lon)
+                    self.cursor.execute(sql_command)
+                    self.connection.commit()
+                except Exception as e:
+                    self.write_log(e)
+
+
+if __name__ == '__main__':
+    try:
+        # 连接本地数据库
+        database = CloudDatabase("EmotionMap", "postgres", "postgres", "127.0.0.1")
+        database.db_connect()
+        database.import_site(r'D:\Users\KYH\Desktop\EmotionMap\RankingSites\site_info.txt')
+    except Exception as e:
+        with open("log.txt", 'a') as log_file:
+            log_file.writelines(str(e))
---
+++ 1SiteRanking/Data Collection/get_flickr_photos.py
@@ -0,0 +1,125 @@
+# coding:utf-8
+# version:python3.5.1
+# author:kyh
+
+import flickrapi
+import datetime
+import psycopg2
+
+
+# flickr照片类
+class flickr_photo(object):
+    def __init__(self, photo_id, photo_site, photo_url):
+        self.id = photo_id
+        self.site = photo_site
+        self.url = photo_url
+
+    # 将照片插入数据库
+    def insert_db(self, db_connection, db_cursor):
+        try:
+            sql_command_insert = "INSERT INTO photo(id,url,site) VALUES({0},'{1}','{2}')".format(self.id,
+                                                                                                 self.url,
+                                                                                                 self.site
+                                                                                                 )
+            db_cursor.execute(sql_command_insert)
+            db_connection.commit()
+            return True
+        except Exception as e:
+            print(e)
+            db_connection.rollback()
+            return False
+
+
+# 连接数据库
+def db_connect():
+    try:
+        connection = psycopg2.connect(database="EmotionMap", user="postgres",
+                                      password="postgres", host="127.0.0.1", port="5432")
+        cursor = connection.cursor()
+        print("Database Connection has been opened completely!")
+        return connection, cursor
+    except Exception as e:
+        print(e)
+
+
+# 查询需要挖掘数据的地点
+def query_site(db_connection, db_cursor):
+    sql_command_select = "SELECT * FROM site WHERE start_query='FALSE' LIMIT 1"
+    db_cursor.execute(sql_command_select)
+    site = db_cursor.fetchone()
+    # 如果存在这样的地点,记录经纬度进行挖掘
+    if site is not None:
+        site_name = site[1]
+        lat = site[2]
+        lon = site[3]
+        sql_command_update = "UPDATE site SET start_query='TRUE' WHERE site_name='{0}'".format(site_name)
+        db_cursor.execute(sql_command_update)
+        db_connection.commit()
+        return site_name, lat, lon
+    # 不存在这样的地点,说明已经全部挖掘完毕
+    else:
+        return None, None, None
+
+
+# flickr api信息
+def flickrAPI():
+    api_key = u'199ed59000c39dd0844b59d01fa7570c'
+    api_secret = u'4a2ce28f1bb8a1fe'
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, cache=True)
+    return flickr
+
+
+# 计算时间
+def compute_time(db_connection, db_cursor, site, latitude, longitude):
+    DATE=datetime.date(2012,1,1)
+    while(True):
+        DATE2=DATE+datetime.timedelta(days=10)
+        datemin ="{0}-{1}-{2}".format(DATE.year,DATE.month,DATE.day)
+        datemax ="{0}-{1}-{2}".format(DATE2.year,DATE2.month,DATE2.day)
+        DATE=DATE+datetime.timedelta(days=10)
+        get_photo_from_location(db_connection, db_cursor, site, latitude, longitude, datemin, datemax)
+        if DATE.year==2017 and DATE.month==12:
+            break
+
+# 获取照片
+def get_photo_from_location(db_connection, db_cursor, site, latitude, longitude, datemin, datemax):
+    flickr = flickrAPI()
+    # 获取所有图片
+    try:
+        photos = flickr.walk(lat=latitude, lon=longitude, radius=3,
+                             min_taken_date=datemin, max_taken_date=datemax, per_page=500, extras='url_c')
+    except Exception as e:
+        print(e)
+    # 获取每一张图片
+    try:
+        for photo_url in photos:
+            url = photo_url.get('url_c')
+            # 如果url不为空,将该图片插入数据库
+            if url is not None:
+                photo_id = int(photo_url.get('id'))
+                photo = flickr_photo(photo_id, site, url)
+                if photo.insert_db(db_connection, db_cursor):
+                    print("Success! Photo id:" + str(photo_id) + "\tPhoto url:" + url)
+    except Exception as e:
+        print(e)
+
+
+# 关闭数据库
+def close_connection(connection):
+    try:
+        connection.close()
+        print("Database Connection has been closed completely!")
+        return True
+    except Exception as e:
+        print(e)
+
+
+# 主操作步骤
+if __name__ == '__main__':
+    db_connection, db_cursor = db_connect()
+    site, lat, lon= query_site(db_connection, db_cursor)
+    if site is not None:
+        compute_time(db_connection, db_cursor, site, lat, lon)
+        close_connection(db_connection, site)
+    else:
+        print("All sites have been recorded!")
